43cbd369aef802177bddf8e3eca91f0a8a997d31
Raphael Quinones <wapquinones28@gmail.com>
2022-10-06 02:27:11 +0800

    init

---
+++ Exercise 2/timer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "timer.hpp"
+
+int main() {
+    //create a timer object
+    timer stopwatch;
+
+    // start timer
+    stopwatch.start();
+    //Compute sum of first ten million positive integers
+    
+}
