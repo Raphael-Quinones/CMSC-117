e44c1b709ca8943728d852551ce634b58069a6d2
Raphael Quinones <wapquinones28@gmail.com>
2022-11-15 23:40:35 +0800

    Finished

--- Exercise 4/item2.py
+++ Exercise 4/item2.py
@@ -6,14 +6,21 @@ plt.style.use(
 )
 
 def f(x):
-    return 1/(1+x**2)
+    return 1/(1-x**2+x**4)
 
 def p_n(z):
-    x = np.linspace(-5,5,5)
+    x = np.linspace(-1,1,20 )
     fx = f(x)
     return polyinterp.LagrangeInterp(fx,x,z)
 
 def g(x):
     return f(x)-p_n(x)
 
-print("supnorm = ", polyinterp.supnorm(g, 1000, -5, 5))
+print("supnorm = ", polyinterp.supnorm(g, 1000, -1, 1))
+z = np.linspace(-1,1, 100)
+fz = f(z)
+Pz = p_n(z)
+plt.figure(1)
+plt.plot(z, fz, '-ok', marker = " ", label = 'f')
+plt.plot(z, Pz, '-ok', marker = " ", color = 'b', label = 'P')
+plt.show()
